f4b0c0f79c68d19466b74540bae38dd5a991e999
italorodrigues <italosport110@hotmail.com>
2024-09-01 15:49:01 -0300

    feat: add eslint and prettier

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,20 @@
-import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import React from "react";
-import { Button, View } from "react-native";
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { Button, View } from 'react-native';
 
 export function NavBar({ navigation }: BottomTabBarProps) {
   return (
     <View
       style={{
-        flexDirection: "row",
-        justifyContent: "space-around",
+        flexDirection: 'row',
+        justifyContent: 'space-around',
         zIndex: 9999,
       }}
     >
-      <Button onPress={() => navigation.navigate("Home")} title="Home" />
-      <Button onPress={() => navigation.navigate("Map")} title="Mapa" />
-      <Button onPress={() => navigation.navigate("Save")} title="Salvos" />
-      <Button onPress={() => navigation.navigate("Profile")} title="Perfil" />
+      <Button onPress={() => navigation.navigate('Home')} title="Home" />
+      <Button onPress={() => navigation.navigate('Map')} title="Mapa" />
+      <Button onPress={() => navigation.navigate('Save')} title="Salvos" />
+      <Button onPress={() => navigation.navigate('Profile')} title="Perfil" />
     </View>
   );
 }
