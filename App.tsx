8ad33ed4ee2570ff41bde8d1a146d5b3497ce431
italorodrigues <italosport110@hotmail.com>
2024-09-02 21:13:10 -0300

    feat: add styled and remove eslint file

--- App.tsx
+++ App.tsx
@@ -1,29 +1,27 @@
-import * as React from "react";
-import { createStaticNavigation } from "@react-navigation/native";
-import {
-  BottomTabBarProps,
-  createBottomTabNavigator,
-} from "@react-navigation/bottom-tabs";
-import { Start } from "@/app/start";
-import { Login } from "@/app/login";
-import { Home } from "@/app/home";
-import { NavBar } from "@/components/Navbar";
-import { Map } from "@/app/map";
-import { Profile } from "@/app/profile";
-import { Save } from "@/app/save";
+import * as React from 'react';
+import { createStaticNavigation } from '@react-navigation/native';
+import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { StartScreen } from '@/app/start';
+import { Login } from '@/app/login';
+import { Home } from '@/app/home';
+import { NavBar } from '@/components/Navbar';
+import { MapScreen } from '@/app/map';
+import { Profile } from '@/app/profile';
+import { Save } from '@/app/save';
 
 const RootStack = createBottomTabNavigator({
   tabBar: (props: BottomTabBarProps) => <NavBar {...props} />,
   screens: {
-    Start: Start,
+    Start: StartScreen,
     Login: Login,
     Home: Home,
-    Map: Map,
+    Map: MapScreen,
     Profile: Profile,
     Save: Save,
   },
   screenOptions: {
-    animation: "fade",
+    animation: 'fade',
   },
 });
 
